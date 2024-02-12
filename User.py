9f06063e97df9933fff84f247640995b847d4831
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-02-12 10:24:03 +0100

    Table user

---
+++ User.py
@@ -0,0 +1,42 @@
+from DB import DB
+
+class User(DB):
+    def __init__(self, host, user, password, database):
+        super().__init__(host, user, password, database)
+        self.data_list = []
+        
+    def create(self,nom,prenom,email,mdp):
+        query = "INSERT INTO user (nom,prenom,email,mdp) VALUES (%s,%s,%s,%s)"
+        param = (nom,prenom,email,mdp)
+        self.executeQuery(query,param)
+    
+    def read(self):
+        query = "SELECT * FROM user"
+        self.data_list = []
+        for line in self.fetch(query):
+            self.data_list.append(line)
+    
+    def delete(self,id):
+        query = "DELETE FROM user WHERE id = %s"
+        param = (id,)
+        self.executeQuery(query,param)
+    
+    def update_nom(self,id,nom):
+        query = "UPDATE user SET nom = %s WHERE id = %s"
+        param = (nom,id)
+        self.executeQuery(query,param)
+    
+    def update_prenom(self,id,prenom):
+        query = "UPDATE user SET prenom = %s WHERE id = %s"
+        param = (prenom,id)
+        self.executeQuery(query,param)
+    
+    def update_email(self,id,email):
+        query = "UPDATE user SET email = %s WHERE id = %s"
+        param = (email,id)
+        self.executeQuery(query,param)
+
+    def update_mdp(self,id,mdp):
+        query = "UPDATE user SET mdp = %s WHERE id = %s"
+        param = (mdp,id)
+        self.executeQuery(query,param)
