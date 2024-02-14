3f9c7890a6e80ca5d25a3a695b73cf0910d42b4c
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-02-14 12:59:44 +0100

    update table

--- Data/chanel.py
+++ Data/chanel.py
@@ -1,37 +1,37 @@
 from DB import DB
 
-class serveur(DB):
+class chanel(DB):
     def __init__(self, host, user, password, database):
         super().__init__(host, user, password, database)
         self.data_liste = []
     
     def create(self,nom):
-        query = "INSERT INTO serveur (nom) VALUES (%s)"
+        query = "INSERT INTO chanel (nom) VALUES (%s)"
         param = (nom,)
         self.executeQuery(query,param)
     
     def read(self):
-        query = "SELECT * FROM serveur"
+        query = "SELECT * FROM chanel"
         self.data_list = []
         for line in self.fetch(query):
             self.data_list.append(line)
     
     def delete(self,id):
-        query = "DELETE FROM serveur WHERE id = %s"
+        query = "DELETE FROM chanel WHERE id = %s"
         param = (id,)
         self.executeQuery(query,param)
     
     def update_nom(self,id,nom):
-        query = "UPDATE serveur SET nom = %s WHERE id = %s"
+        query = "UPDATE chanel SET nom = %s WHERE id = %s"
         param = (nom,id)
         self.executeQuery(query,param)
     
     def get_id_by_nom(self,nom):
-        query = "SELECT id FROM serveur WHERE nom = %s"
+        query = "SELECT id FROM chanel WHERE nom = %s"
         param = (nom,)
         return self.fetch(query,param)
 
     def get_nom_by_id(self,id):
-        query = "SELECT nom FROM serveur WHERE id = %s"
+        query = "SELECT nom FROM chanel WHERE id = %s"
         param = (id,)
         return self.fetch(query,param)
