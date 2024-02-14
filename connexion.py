07ff6678b37d456216089829f74075ef902b6042
SK4 <valentin.beaussaert@laplateforme.io>
2024-02-14 14:31:21 +0100

    upload connexion.py

--- connexion.py
+++ connexion.py
@@ -5,26 +5,22 @@ class Connexion:
     def __init__(self, master, switch_function):
         self.master = master
         self.master.title("Page de Connexion")
-        self.master.configure(bg='#0A3D62')  # Appliquer la couleur de fond bleu nocturne
+        self.master.configure(bg='#0A3D62')  # Couleur de fond bleu nocturne
 
-
-        # Créer un cadre pour contenir les widgets de connexion
         frame = tk.Frame(master, bg='#0A3D62')
-        frame.pack(expand=True)
+        frame.pack(expand=True, padx=100, pady=100)  # Ajuster l'espacement
 
-        # Champ adresse mail dans le cadre
-        tk.Label(frame, text="Adresse mail :", bg='#0A3D62', fg='white').grid(row=0, column=0)
-        self.adresse_mail_connexion = tk.Entry(frame, width=30)
-        self.adresse_mail_connexion.grid(row=0, column=1)
+        # Configure les widgets
+        tk.Label(frame, text="Adresse mail :", bg='#0A3D62', fg='white').grid(row=0, column=0, pady=(10, 0))
+        self.adresse_mail_connexion = tk.Entry(frame, width=30, bg='#0A3D62', fg='white', borderwidth=0)
+        self.adresse_mail_connexion.grid(row=0, column=1, pady=(10, 0))
 
-        # Champ mot de passe dans le cadre
-        tk.Label(frame, text="Mot de passe :", bg='#0A3D62', fg='white').grid(row=1, column=0)
-        self.mot_de_passe_connexion = tk.Entry(frame, show="*", width=30)
-        self.mot_de_passe_connexion.grid(row=1, column=1)
+        tk.Label(frame, text="Mot de passe :", bg='#0A3D62', fg='white').grid(row=1, column=0, pady=(10, 0))
+        self.mot_de_passe_connexion = tk.Entry(frame, show="*", width=30, bg='#0A3D62', fg='white', borderwidth=0)
+        self.mot_de_passe_connexion.grid(row=1, column=1, pady=(10, 0))
 
-        # Boutons dans le cadre
-        tk.Button(frame, text="Connexion", command=self.soumettre_connexion, width=30, bg='#0A3D62', fg='white').grid(row=2, column=0, sticky=tk.W)
-        tk.Button(frame, text="S'inscrire", command=lambda: switch_function(master), bg='#0A3D62', fg='white', width=30,).grid(row=2, column=1, sticky=tk.E)
+        tk.Button(frame, text="Connexion", command=self.soumettre_connexion, bg='#0A3D62', fg='white', width=20).grid(row=2, column=0, padx=10, pady=10, sticky=tk.W)
+        tk.Button(frame, text="S'inscrire", command=lambda: switch_function(master), bg='#0A3D62', fg='white', width=20).grid(row=2, column=1, padx=10, pady=10, sticky=tk.E)
 
     def soumettre_connexion(self):
         email = self.adresse_mail_connexion.get()
