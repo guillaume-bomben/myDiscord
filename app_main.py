7d26045e015dc95ed6165eb494cf0ab27d70aee4
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-02-27 15:04:40 +0100

    create clas to manage all class

---
+++ app_main.py
@@ -0,0 +1,22 @@
+from connexion import Connexion
+from inscription import Inscription
+from chat import chat
+
+class app_main:
+    def __init__(self):
+        self.Connect_page = Connexion()
+        
+        self.event()
+        
+    
+    def event(self):
+        while True:
+            if self.Connect_page.chat:
+                self.chat_page = chat(self.Connect_page.id)
+                self.Connect_page.chat = False
+            if self.Connect_page.inscription:
+                self.inscription_page = Inscription()
+                self.Connect_page.inscription = False
+            if self.chat_page.connexion == False:
+                self.Connect_page = Connexion()
+                self.chat_page.connexion = True
