bfd6043cc24c5dd7173bb242a6a9eaaaf973cb4b
guillaume-bomben <guillaume.bomben@laplateforme.io>
2024-02-27 15:04:47 +0100

    cleaning

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-import tkinter as tk
-from connexion import Connexion
-from inscription import Inscription
+from app_main import app_main
 
-def main():
-    root = tk.Tk()
-    Connexion(root, afficher_inscription)  # Simplified initialization
-    root.mainloop()
-
-def afficher_inscription(master):
-    master.withdraw()  # Hide the login window
-    inscription_window = tk.Toplevel(master)  # Ensures correct parent-child relationship
-    Inscription(inscription_window, lambda: master.destroy())  # Simplified lambda function
-
-if __name__ == "__main__":
-    main()
+app = app_main()
